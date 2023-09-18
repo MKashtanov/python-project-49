7d10729a21f8100e94397d4d55fa51847ce39591
MKashtanov <MKashtanov@mail.ru>
2023-09-18 15:58:26 +0400

    fix. Refactoring Progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,25 +2,34 @@ from brain_games.games.common import get_random_number
 from brain_games.games.common import get_user_answer
 from brain_games.games.common import check_answer
 
+MAX_STEP = 10
+MAX_LEN_PROGRESSION = 10
+MIN_LEN_PROGRESSION = 5
+
 
 def print_rules():
     print('What number is missing in the progression?')
 
 
-def run_game(max_num=100):
-    max_step = 10
-    step = get_random_number(max_step)
-    len_progression = 4 + get_random_number(6)  # длина 5-10 чисел
-    hidden_position = get_random_number(len_progression)
+def generate_progression(max_num):
+    step = get_random_number(MAX_STEP)
+    len_progression = (MIN_LEN_PROGRESSION - 1
+                       + get_random_number(MAX_LEN_PROGRESSION
+                                           - MIN_LEN_PROGRESSION + 1))
     first_num = get_random_number(max_num)
     progression = []
-    current_num = first_num
-
-    while len(progression) < len_progression:
+    for current_num in range(first_num,
+                             first_num + (len_progression * step), step):
         progression.append(str(current_num))
-        current_num += step
-    right_answer = str(progression[hidden_position - 1])
-    progression[hidden_position - 1] = '..'
+    return progression
+
+
+def run_game(max_num=100):
+    progression = generate_progression(max_num)
+    hidden_position = get_random_number(len(progression)) - 1
+
+    right_answer = str(progression[hidden_position])
+    progression[hidden_position] = '..'
 
     question = ' '.join(progression)
     user_answer = get_user_answer(question)
